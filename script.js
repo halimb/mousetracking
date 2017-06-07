16c07d2f95ff8db9510a8ad92ff992c20e82b4bf
Halim <halim.bouguedra@gmail.com>
2017-06-07 01:01:23 +0100

    blank prototype

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("yo");
+var canvas = document.getElementById('cnv');
+var w = window.innerWidth;
+canvas.width = w / 2;
+canvas.height = w / 2;
+var ctx = canvas.getContext('2d');
+console.log([ctx]);
+
+// ctx.fillStyle = 'rgba(0,0,0,1)';
+// ctx.fillRect(0,0,200,200);
+initCanvas();
+
+function initCanvas() {
+	ctx.fillRect(canvas.width / 2 - 2, canvas.height / 2 - 2, 4, 4);
+}
+
+canvas.onclick = function(e) {
+			var x = e.clientX - canvas.offsetLeft;
+			var y = e.clientY - canvas.offsetTop;
+			ctx.fillRect(x - 10, y - 10, 20, 20)
+		}
