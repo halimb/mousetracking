85808feb2154f6904768d52f3fde37beee036108
Halim <halim.bouguedra@gmail.com>
2017-06-07 17:46:18 +0100

    slash prototype

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var divs = document.getElementsByClassName('div');
+var prev = Array(divs.length).fill(0);
+var laps = Array(divs.length).fill(0);
 var w = window.innerWidth;
 var h = window.innerHeight;
 var xpos = 0, ypos = 0;
@@ -10,7 +12,7 @@ function Point(x, y) {
 	this.getDist = function(xb, yb) {
 		var distance = 
 			Math.sqrt(
-				Math.pow( (this.y - yb), 2 ) +
+				Math.pow( (yb - this.y), 2 ) +
 				Math.pow( (xb - this.x), 2 )
 			);
 		return distance;
@@ -19,38 +21,42 @@ function Point(x, y) {
 	this.angleTo = function(xb, yb) {
 		var dist = this.getDist(xb, yb);
 		var normX = (xb - this.x) / dist;
-		var angle = -Math.acos(normX);
-		angle = (this.y - yb < 0) ? 
-				 -angle : angle;
-		return -angle;
+		var normY = (this.y - yb) / dist;
+		var acos = parseInt( Math.acos(normX) * 180 / Math.PI);
+		var asin = parseInt( Math.asin(normY) * 180 / Math.PI);
+		var angle = (normY > 0) ? acos : 
+						((normX < 0) ? 180 - asin : 
+										360 + asin);
+		return angle;
 	}
 
 }
 
 document.onmousemove = function(e){
-				ypos = e.clientX;
-				xpos = e.clientY;
+				xpos = e.clientX;
+				ypos = e.clientY;
 				rotateTo(xpos, ypos);
 			}
 
-
+var display = document.getElementById("display");
 
 function rotateTo(a, b) {
 	for(var i = 0; i < divs.length; i++) {
-
 		var div = divs[i]
 		var dim = div.offsetHeight;
-		var x = div.offsetTop + dim / 2;
-		var y = div.offsetLeft + dim / 2;
+		var x = div.offsetLeft + dim / 2;
+		var y = div.offsetTop + dim / 2;
 		var p = new Point(x, y);
-		var rad = p.angleTo(a, b);
+		var curr = p.angleTo(a, b);
 
-		var x = p.x - dim/2;
-		var y = p.y - dim/2;
-		var deg = rad * 180 / Math.PI;
-		div.style.transform = 'rotate(' + deg + 'deg)';
+		if(div.id =="test") 
+		{
+			var x = p.x - dim/2;
+			var y = p.y - dim/2;
+			div.style.transform = 'rotate(' + -curr + 'deg)';
+			//console.log(curr)
+		}
 	}
 }
-
 
 
