c7038f85503f8de51513866a87e94c6175945179
Halim <halim.bouguedra@gmail.com>
2017-06-07 03:12:23 +0100

    first working prototype

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
-var canvas = document.getElementById('cnv');
+var divs = document.getElementsByClassName('div');
 var w = window.innerWidth;
-canvas.width = w / 2;
-canvas.height = w / 2;
-var ctx = canvas.getContext('2d');
-console.log([ctx]);
+var h = window.innerHeight;
+var xpos = 0, ypos = 0;
 
-// ctx.fillStyle = 'rgba(0,0,0,1)';
-// ctx.fillRect(0,0,200,200);
-initCanvas();
+function Point(x, y) {
+	this.x = x;
+	this.y = y;
 
-function initCanvas() {
-	ctx.fillRect(canvas.width / 2 - 2, canvas.height / 2 - 2, 4, 4);
+	this.getDist = function(xb, yb) {
+		var distance = 
+			Math.sqrt(
+				Math.pow( (this.y - yb), 2 ) +
+				Math.pow( (xb - this.x), 2 )
+			);
+		return distance;
+	}
+
+	this.angleTo = function(xb, yb) {
+		var dist = this.getDist(xb, yb);
+		var normX = (xb - this.x) / dist;
+		var angle = -Math.acos(normX);
+		angle = (this.y - yb < 0) ? 
+				 -angle : angle;
+		return -angle;
+	}
+
 }
 
-canvas.onclick = function(e) {
-			var x = e.clientX - canvas.offsetLeft;
-			var y = e.clientY - canvas.offsetTop;
-			ctx.fillRect(x - 10, y - 10, 20, 20)
-		}
+document.onmousemove = function(e){
+				ypos = e.clientX;
+				xpos = e.clientY;
+				rotateTo(xpos, ypos);
+			}
+
+
+
+function rotateTo(a, b) {
+	for(var i = 0; i < divs.length; i++) {
+
+		var div = divs[i]
+		var dim = div.offsetHeight;
+		var x = div.offsetTop + dim / 2;
+		var y = div.offsetLeft + dim / 2;
+		var p = new Point(x, y);
+		var rad = p.angleTo(a, b);
+
+		var x = p.x - dim/2;
+		var y = p.y - dim/2;
+		var deg = rad * 180 / Math.PI;
+		div.style.transform = 'rotate(' + deg + 'deg)';
+	}
+}
+
+
+
