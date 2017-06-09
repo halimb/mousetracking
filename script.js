0e22f2bb2f840f79f41a16f8406b519875cfb700
Halim <halim.bouguedra@gmail.com>
2017-06-09 18:32:34 +0100

    remove rotation bug handling

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const dim = 60;
 const GUTTER = 80;
+
 var xpos = 0, ypos = 0;
-var prevXpos = 0, prevYpos = 0;
 var grid = document.getElementById('grid');
-var rows, cellDim, cells, prev, laps, 
-	prevYs, maxDist, w, h, ROWS, COLS;
+var rows, cellDim, cells, w, h, ROWS, COLS;
 var t = 0;
 
 function Point(x, y) {
@@ -38,7 +37,6 @@ init();
  * inflate the right number of divs in order  *
  * to fill the whole screen                   */
 function init() {
-	console.log("init()")
 	grid.innerHTML = "";
 	w = window.innerWidth;
 	h = window.innerHeight
@@ -59,13 +57,6 @@ function init() {
 		}
 	}
 	cells = document.querySelectorAll(".cell");
-	prev = Array(cells.length).fill(0);
-	laps = Array(cells.length).fill(0);
-	prevYs = Array(cells.length).fill(0);
-	maxDist = Math.sqrt(
-				Math.pow( document.body.offsetWidth, 2 ) +
-				Math.pow( document.body.offsetHeight, 2 )
-			);
 	rotateTo(w/2, h/2);
 }; 
 
@@ -78,44 +69,16 @@ window.onresize = function() {
 document.onmousemove = function(e){
 				xpos = e.clientX;
 				ypos = e.clientY;
-				if( Math.abs(prevXpos - xpos) > 10||
-					Math.abs(prevYpos - ypos) > 10 ) {
-					prevXpos = xpos;
-					prevYpos = ypos;
-					rotateTo(xpos, ypos);
-				}
+				rotateTo(xpos, ypos);
 			}
 
 function rotateTo(a, b) {
 	for(var i = 0; i < cells.length; i++) {
 		var div = cells[i]
-		var dim = div.offsetHeight;
 		var x = div.offsetLeft + div.offsetWidth / 2;
 		var y = div.offsetTop + div.offsetHeight / 2;
 		var p = new Point(x, y);
-		var curr = p.angleTo(a, b) + laps[i] * 360;
-
-		/*Uncomment this block if transitions	*
-		  are enabled in css. this forces the 	*
-		  divs to always choose the shortest 	*
-		  rotation path							*/
-		var dir = prevYs[i] - y;
-		if(Math.abs(prev[i] - curr) > 180) {
-			curr = dir > 0 ? 
-				curr + 360 : -360 + curr;
-			laps[i] += dir > 0 ? 1 : -1;
-		}
-		prevYs[i] = b;
-		prev[i] = curr;
-
-		var normalDist = p.getDist(a, b) / maxDist;
-		var c = 255// - Math.floor(100 * normalDist) ;
-		var color = 'rgba(' + c + ',' + c + ',' + c + ', 1)';
-		var d = ".1vw"//(parseFloat(.3 + normalDist * 6).toFixed(4)) + 'vw';
-
-		var border = d + ' solid ' + color;
-		div.style.borderTop = border;
-
+		var curr = p.angleTo(a, b);
 		div.style.transform = 'rotate(' + -curr + 'deg)';
 	}
 }
